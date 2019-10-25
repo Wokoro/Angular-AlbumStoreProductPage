7d846a779ec0ab1b370001fa536b1a6ab2272212
Wokoro Douye Samuel <wokorosamuel@yahoo.com>
2019-10-25 07:10:20 -0700

    Completed challenge 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Product } from './product';
 import 'rxjs/add/operator/map'
 
 import { Album } from './album'
@@ -9,11 +10,17 @@ import { Observable } from 'rxjs/Observable';
 export class ProductService {
 
   private _albumUrl: string = '../assets/album.json';
+  private _productsUrl:string = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album>{
     return this._http.get(this._albumUrl)
-    .map((response) => <Album>response.json());
+    .map( response => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]>{
+   return this._http.get( this._productsUrl)
+   .map(response => <Product[]>response.json());
   }
 }
